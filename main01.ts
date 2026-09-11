31a73966ca973fb10339af9f868c0b9514cb21f4
nicollaslopes08 <nicollas.lopes@edu.senai.br>
2026-09-11 20:08:04 -0300

    main01.ts

--- main01.ts
+++ main01.ts
@@ -1,11 +1,11 @@
 import { Pessoa } from "./exercicio01";
 
-//Criando objetos da classe Pessoa
+
 
 const pessoa1 = new Pessoa();
 const pessoa2 = new Pessoa();
 
-//objeto 1
+
 pessoa1.setNome("Tom Cruise");
 pessoa1.setIdade(60);
 pessoa1.setEndereco("California, USA");
@@ -13,14 +13,14 @@ pessoa1.setProfissao("Ator");
 pessoa1.setCPF("CPF: 12345678990");
 pessoa1.setRG("RG: 23456789001")
 
-//objeto 2
+
 pessoa2.setNome ("Messi");
 pessoa2.setIdade(35);
 pessoa2.setEndereco("Miami, USA");
 pessoa2.setProfissao("Jogador de futebol");
 pessoa2.setCPF("CPF: 32464538793");
 pessoa2.setRG("RG: 346427853");
-// Chamando os metodos get() para apresentar os objetos
+
 
 console.log("-----objeto 1-----");
 console.log(pessoa1.getNome());
